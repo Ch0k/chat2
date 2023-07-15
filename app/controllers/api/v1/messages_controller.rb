afe7c833425a9b1d633700218f7da7e24e6115d8
Andreev Sergey <andreev.sv89@yandex.ru>
2023-07-15 13:07:03 +0300

    finish message controller

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class MessagesController < Api::V1::BaseController
+      before_action :get_mailbox
+      
+      def create
+        conversation = Mailboxer::Conversation.find(params[:conversation_id])
+        body = params[:body]
+        @message = current_user.reply_to_conversation(conversation, body)
+        render json: @message, status: :created
+      end
+
+      def show
+        byebug
+        @message = Mailboxer::Message.find(params[:id])
+        @conversation = @message&.conversation
+        if @message.present? && @conversation.present?
+          if @conversation.is_participant?(current_user)
+            render json: @message, status: :ok
+          end
+        end
+      end
+
+      private
+
+      def get_mailbox
+        @mailbox = current_user.mailbox
+      end
+    end
+  end
+end
