cf40e034eed2b619e7129713b90708e6443bb3f6
Andreev Sergey <andreev.sv89@yandex.ru>
2023-07-15 13:12:13 +0300

    added some method for working lib

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,23 @@ class User < ApplicationRecord
   api_guard_associations refresh_token: 'refresh_tokens'
   has_many :refresh_tokens, dependent: :delete_all
 
+  acts_as_messageable
+
   def authenticate(password)
     if self.valid_password?(password)
       self
     end
+  end
+
+  def mailboxer_email(object)
+    #Check if an email should be sent for that object
+    #if true
+    return "define_email@on_your.model"
+    #if false
+    #return nil
+  end
+
+  def name
+    return "You should add method :name in your Messageable model"
   end
 end
